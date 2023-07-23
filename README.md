2faa3fefcb4ce7e00a092671da37a35d38e4f484
easyhutu <mengjianhua@bilibili.com>
2023-07-23 12:42:42 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # any sync
 
-局域网同步工具
+局域网同步工具，不需要登录QQ、微信即可在局域网内跨平台同步数据
 
 > PC端
 
@@ -15,8 +15,17 @@
 
 ### [下载对应可执行文件](https://github.com/easyhutu/any-sync/releases)
 
-### 克隆项目
+### DEV
+
+> 项目依赖 go 1.20
+
+项目没有外漏配置参数，如需修改配置信息请查看 [/app/config/config.go](/app/config/config.go)
+
+
 ```shell
 go mod tidy
 go build -o anysync && anysync
 ```
+
+***
+#### *注意：文件同步未做任何加密和鉴权处理，使用中请注意隐私保护*
--- app/config/config.go
+++ app/config/config.go
@@ -9,20 +9,20 @@ import (
 type Config struct {
 	BoundIp          string
 	ListenPort       int
-	PingSecond       time.Duration
+	PingSecond       time.Duration // 超过心跳间隔删除设备
 	ShareUrl         string
-	QrSize           int
-	MaxUploadFiles   int64
-	ShareFilesPrefix string
+	QrSize           int    // 分享二维码size
+	MaxUploadFiles   int64  // 最大上传文件大小 Mib
+	ShareFilesPrefix string // 上传文件公共目录
 }
 
 func NewConfig() (config *Config, err error) {
 	config = &Config{
 		ListenPort:       8080,
-		PingSecond:       time.Minute * 16, // 超过心跳间隔删除设备
-		QrSize:           260,              // 分享二维码size
-		MaxUploadFiles:   1025 << 20,       // 最大上传文件大小 Mib
-		ShareFilesPrefix: "anySyncShare",   // 上传文件公共目录
+		PingSecond:       time.Minute * 16,
+		QrSize:           260,
+		MaxUploadFiles:   1025 << 20,
+		ShareFilesPrefix: "anySyncShare",
 	}
 	config.BoundIp = device.GetBoundIp()
 	config.ShareUrl = fmt.Sprintf("http://%s:%d/mobile", config.BoundIp, config.ListenPort)
